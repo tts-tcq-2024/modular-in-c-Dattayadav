560b7aac84e2cfff0d580f30fe346f4b8d036a12
Dattayadav <87682187+Dattayadav@users.noreply.github.com>
2024-09-26 10:59:01 +0530

    Update colorPair.h

--- colorPair.h
+++ colorPair.h
@@ -14,5 +14,5 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-
+void printColorCodeReferenceManual(int numberOfMajorColors, int numberOfMinorColors);
 #endif // COLOR_PAIR_H
